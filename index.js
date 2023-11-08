c27e194e2fbec24b9052d9071107fe39699bb419
Shawon Mahmud <shawon.phero@gmail.com>
2023-11-08 19:28:41 +0600

    get book categories using get method

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const categoriesCollection = client
+      .db("libraryDB")
+      .collection("categories");
+
+    //get book categories using get method
+    app.get("/categories", async (req, res) => {
+      const cursor = categoriesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +42,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
